387e242b00f73492edd49b83007422786224e3b2
Sam (csumb) <sogden@csumb.edu>
2023-04-17 22:05:15 -0700

    Updating server code

--- PA2/src/server.py
+++ PA2/src/server.py
@@ -1,38 +1,35 @@
 #!env python
-
+import os.path
 import socket as s
-import sys # In order to terminate the program
+import sys
 
-serverSocket = s.socket(AF_INET,SOCK_STREAM)
-#Prepare a server socket
-#Fill in start
-#Fill in end
+serverSocket = s.socket(s.AF_INET,s.SOCK_STREAM)
+# todo: Prepare a server socket
 
 while True:
   #Establish the connection
-   print('Ready to serve...')
-   connectionSocket, addr =   #Fill in start              #Fill in end
-   try:
-    message =   #Fill in start          #Fill in end
-    filename = message.split()[1]
-    f = open(filename[1:])
-    outputdata = #Fill in start       #Fill in end
-    #Send one HTTP header line into socket
-    #Fill in start
-    #Fill in end
-    #Send the content of the requested file to the client
-    for i in range(0, len(outputdata)):
-      connectionSocket.send(outputdata[i].encode())
-      connectionSocket.send("\r\n".encode())
-      connectionSocket.close()
-  except IOError:
-    #Send response message for file not found
-    #Fill in start
-    #Fill in end
-    #Close client socket
-    #Fill in start
-    #Fill in end
+  print('Ready to serve...')
+  connection_socket, addr = None, None # todo: Accept a connection
 
-serverSocket.close()
-sys.exit()#Terminate the
+  message = None # todo: accept message from socket
+  file_name = message.split()[1]
+  if os.path.exists(file_name):
+    # File exists so read it and send it
+    with open(file_name[1:]) as fid:
+      output_data = None # todo: read file to prepare to send over the network
+    
+    # todo: Send HTTP header file
+    
+    #Send the content of the requested file to the client
+    for i in range(0, len(output_data)):
+      connection_socket.send(output_data[i].encode())
+      connection_socket.send("\r\n".encode())
+  else:
+    # File does not exist on server
+    pass
+    # todo: Send appropriate response message for file not found
+  
+  # Close the connection socket
+  connection_socket.close()
+  
 
