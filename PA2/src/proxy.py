48b69f41c1b54181c6531d4cbc07772a77a23564
Sam (csumb) <sogden@csumb.edu>
2023-04-17 21:57:31 -0700

    Adding an almost total rewrite of proxy.py

--- PA2/src/proxy.py
+++ PA2/src/proxy.py
@@ -2,69 +2,57 @@
 
 import socket as s
 import sys
+import os
 
 if len(sys.argv) <= 1:
-  print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
+  print('Usage : "python ProxyServer.py [server_ip]"\n[server_ip : It is the IP Address Of Proxy Server')
   sys.exit(2)
 
-# Create a server socket, bind it to a port and start listening tcpSerSock = socket(AF_INET, SOCK_STREAM)
-# Fill in start.
-# Fill in end.
+# Create a server listening socket, bind it to a port and start listening
+proxy_listening_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+# todo: bind the port appropriately
 
-while 1:
+# Create a socket to our backing server (e.g. server.py), and bind it to a port for when we need it
+proxy_backing_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+# todo: bind the port appropriately
+
+while True:
   # Start receiving data from the client
   print('Ready to serve...')
-  tcpCliSock, addr = tcpSerSock.accept()
+  proxy_client_socket, addr = proxy_listening_socket.accept()
   print('Received a connection from:', addr)
-  message = # Fill in start.    # Fill in end.
+  message = None # todo: add an appropriate message
   print(message)
+  
   # Extract the filename from the given message
   print(message.split()[1])
   filename = message.split()[1].partition("/")[2]
   print(filename)
-  fileExist = "false"
-  filetouse = "/" + filename
-  print(filetouse)
-  try:
-    # Check wether the file exist in the cache
-    f = open(filetouse[1:], "r")
-    outputdata = f.readlines()
-    fileExist = "true"
-    # ProxyServer finds a cache hit and generates a response message
-    tcpCliSock.send("HTTP/1.0 200 OK\r\n")
-    tcpCliSock.send("Content-Type:text/html\r\n")
-    # Fill in start.
-    # Fill in end.
-    print('Read from cache')
-    # Error handling for file not found in cache    except IOError:
-    if fileExist == "false":
-      # Create a socket on the proxyserver
-      c = # Fill in start.    # Fill in end.
-      hostn = filename.replace("www.","",1)
-      print(hostn)
-      try:
-        # Connect to the socket to port 80
-        # Fill in start.
-        # Fill in end.
-    # Create a temporary file on this socket and ask port 80 for the file requested by the client
-    fileobj = c.makefile('r', 0)
+  
+  if not os.path.exists(filename):
+    # File not found in cache so we retrieve it from the remote server
 
-    fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")
-    # Read the response into buffer
-    # Fill in start.
+    # Try to get copy of file from backing server
+    try:
+      # Create a file object for this socket and ask port 80 for the file requested by the client
+      socket_file_obj = c.makefile('r', 0)
+      socket_file_obj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")
+      
+      # todo: Check response and fail gracefully if file wasn't on backing server
+      #  If response was bad, craft an appropriate HTTP response and continue
+      
+      # todo: Read the response into buffer
+      # todo: Create a new file in the cache for the requested file.
+    except Exception as e:
+      print(e)
+      exit(8)
+  
+  # If we make it here, then the file should exist locally
+  # ProxyServer finds a cache hit and generates a response message
+  with open(filename, "r") as fid:
+    output_data = fid.readlines()
 
-    # Fill in end.
-    # Create a new file in the cache for the requested file.
-    # Also send the response in the buffer to client socket and the corresponding file in the cache
-        tmpFile = open("./" + filename,"wb")
-        # Fill in start.              # Fill in end.
-      except:
-        print("Illegal request")
-    else:
-      # HTTP response message for file not found
-      # Fill in start.
-      # Fill in end.
-  # Close the client and the server sockets
-  tcpCliSock.close()
-  # Fill in start.
-  # Fill in end.
+  proxy_client_socket.send("HTTP/1.0 200 OK\r\n")
+  proxy_client_socket.send("Content-Type:text/html\r\n")
+  # todo: generate response using found file
+  
