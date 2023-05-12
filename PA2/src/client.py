2a291157c9ae0f9a182a449335defaf2d94d23cf
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-05-12 09:21:29 -0700

    refactored try block

--- PA2/src/client.py
+++ PA2/src/client.py
@@ -1,23 +1,26 @@
 import socket
 
 # server will always be on mininet h1
-IP = "10.0.0.1"
+SERVER = "10.0.0.1"
 PORT = 12000
+REQUESTS = 10
 socket.setdefaulttimeout(1)
 
 if __name__ == "__main__":
-  print("Pinging mininet h1 [" + IP + "] on port [" + str(PORT) + "]:")
+  print("Pinging server [" + SERVER + "] on port [" + str(PORT) + "] " + str(REQUESTS) + " times:")
   udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   
-  ping_num = 0
+  ping_count = 0
 
-  for i in range(10):
-    ping_num += 1
+  for i in range(REQUESTS):
+    ping_count += 1
+    udp_socket.sendto(("Ping " + str(ping_count)).encode(), (SERVER, PORT))
 
     try:
-      udp_socket.sendto(("Ping " + str(ping_num)).encode(), (IP, PORT))
-
       echoed_ping = udp_socket.recv(4096)
+    except TimeoutError:
+      print("Ping " + str(ping_count) + ": Request timed out")
+    else:
       print(echoed_ping.decode() + ": Success")
-    except:
-      print("Ping " + str(ping_num) + ": Request timed out")
+
+  udp_socket.close()
