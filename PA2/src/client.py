eb9834007feb9dd7b42b07c7e392db334cfefe08
SMCaseyCode <104325289+SMCaseyCode@users.noreply.github.com>
2023-05-13 13:25:47 -0700

    Comments + Capitalized Const Vars

--- PA2/src/client.py
+++ PA2/src/client.py
@@ -3,31 +3,39 @@ import server
 
 import logging
 
+# logging which is not used yet
 logging.basicConfig()
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
 
-server_host = "10.0.0.1"
-server_port = server.PORT_NUMBER
-pings = 10
+# const variables
+SERVER_HOST = "10.0.0.1"
+SERVER_PORT = server.PORT_NUMBER
+PINGS = 10
 
+# default timeout much too long - this reduces it
 socket.setdefaulttimeout(1)
 
 
 def main():
+    # closes socket after use
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
 
-        for x in range(pings):
+        # pings x times (reliant on PING variable)
+        for x in range(PINGS):
             message = "Ping " + str(x + 1) + ": "
-            server_socket.sendto(message.encode(), (server_host, server_port))
+            server_socket.sendto(message.encode(), (SERVER_HOST, SERVER_PORT))
 
+            # Try-Catch block. Failure == timeout exception
             try:
                 response_msg, _ = server_socket.recvfrom(5005)
             except:
+                # Failed
                 print("Ping " + str(x + 1) + " timed out")
             else:
-                print(response_msg.decode() + "Successfully pinged server " + "(" + server_host + ", " + str(
-                    server_port) + ")")
+                # Success
+                print(response_msg.decode() + "Successfully pinged server " + "(" + SERVER_HOST + ", " + str(
+                    SERVER_PORT) + ")")
 
 
 if __name__ == "__main__":
