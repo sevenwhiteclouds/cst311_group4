d6f6b4ba271eb8b620840e39fe9d0144cec55cee
SMCaseyCode <104325289+SMCaseyCode@users.noreply.github.com>
2023-05-13 13:20:16 -0700

    Changed to 'with' for socket

--- PA2/src/client.py
+++ PA2/src/client.py
@@ -15,20 +15,19 @@ socket.setdefaulttimeout(1)
 
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
 
-    for x in range(pings):
-        message = "Ping " + str(x + 1) + ": "
-        server_socket.sendto(message.encode(), (server_host, server_port))
+        for x in range(pings):
+            message = "Ping " + str(x + 1) + ": "
+            server_socket.sendto(message.encode(), (server_host, server_port))
 
-        try:
-            response_msg, _ = server_socket.recvfrom(5005)
-        except:
-            print("Ping " + str(x + 1) + " timed out")
-        else:
-            print(response_msg.decode() + "Successfully pinged server " + "(" + server_host + ", " + str(server_port) + ")")
-
-    server_socket.close()
+            try:
+                response_msg, _ = server_socket.recvfrom(5005)
+            except:
+                print("Ping " + str(x + 1) + " timed out")
+            else:
+                print(response_msg.decode() + "Successfully pinged server " + "(" + server_host + ", " + str(
+                    server_port) + ")")
 
 
 if __name__ == "__main__":
