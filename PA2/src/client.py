175e20aca0a7f325b8e913216c490bccce24f5e0
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-05-13 18:56:00 -0700

    Update client.py

--- PA2/src/client.py
+++ PA2/src/client.py
@@ -1,4 +1,4 @@
-import socket
+import socket as s
 import logging
 import server
 
@@ -7,11 +7,21 @@ log=logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 server_host = "10.0.0.1"
-server_post = 
+server_post = 12000
+ping_nums = 10
 
 def main():
+    client_socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    client_socket.settimeout(1)
 
-
+    for i in range(ping_nums):
+        message = "Ping #".format(i+1)
+        client_socket.sendto(message.encode(),(server_host,server_port))
+    try:
+        msg_reply,_ = client_socket.recvfrom(1024)
+        print(msg_reply.decode())
+    except s.timeout:
+        print("Request #".format(i+1) + "timed out")
 
 if __name__ == "__main__":
     main()
