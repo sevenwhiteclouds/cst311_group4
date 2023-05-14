45676ca6b20d7d8f3d7f491f4b04cfccd4929a9d
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-05-13 18:36:23 -0700

    Create client.py

---
+++ PA2/src/client.py
@@ -0,0 +1,17 @@
+import socket
+import logging
+import server
+
+logging.basicConfig()
+log=logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+server_host = "10.0.0.1"
+server_post = 
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
