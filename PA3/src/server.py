8f82268cfc185804445f116624fa5a2731fba5cb
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-05-24 07:08:46 -0700

    done

--- PA3/src/server.py
+++ PA3/src/server.py
@@ -1,17 +1,7 @@
-#!env python
+import socket
+import threading
+import time
 
-"""Chat server for CST311 Programming Assignment 3"""
-__author__ = "[team name here]"
-__credits__ = [
-  "Your",
-  "Names",
-  "Here"
-]
-
-
-import socket as s
-
-# Configure logging
 import logging
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -19,52 +9,45 @@ log.setLevel(logging.DEBUG)
 
 server_port = 12000
 
-def connection_handler(connection_socket, address):
-  # Read data from the new connectio socket
-  #  Note: if no data has been sent this blocks until there is data
-  query = connection_socket.recv(1024)
-  
-  # Decode data from UTF-8 bytestream
-  query_decoded = query.decode()
-  
-  # Log query information
-  log.info("Recieved query test \"" + str(query_decoded) + "\"")
-  
-  # Perform some server operations on data to generate response
-  response = query_decoded.upper()
-  
-  # Sent response over the network, encoding to UTF-8
-  connection_socket.send(response.encode())
-  
-  # Close client socket
-  connection_socket.close()
-  
+def spawn_thread(connection_socket, address, lock, data):
+  log.info("Connected to client at " + str(address))
 
-def main():
-  # Create a TCP socket
-  # Notice the use of SOCK_STREAM for TCP packets
-  server_socket = s.socket(s.AF_INET,s.SOCK_STREAM)
+  mssg = connection_socket.recv(1024).decode()
+
+  lock.acquire()
+  data.append(mssg)
+  lock.release()
+
+  log.info("Recieved query test \"" + mssg + "\"")
+
+  while len(data) < 2:
+    time.sleep(1)
   
-  # Assign IP address and port number to socket, and bind to chosen port
-  server_socket.bind(('',server_port))
-  
-  # Configure how many requests can be queued on the server at once
-  server_socket.listen(1)
-  
-  # Alert user we are now online
-  log.info("The server is ready to receive on port " + str(server_port))
-  
-  # Surround with a try-finally to ensure we clean up the socket after we're done
-  try:
-    # Enter forever loop to listen for requests
-    while True:
-      # When a client connects, create a new socket and record their address
-      connection_socket, address = server_socket.accept()
-      log.info("Connected to client at " + str(address))
-      # Pass the new socket and address off to a connection handler function
-      connection_handler(connection_socket, address)
-  finally:
-    server_socket.close()
+  response = 'X : "' + data[0] + '", Y : "' + data[1] + '"'
+
+  connection_socket.send(response.encode())
+  connection_socket.close()
 
 if __name__ == "__main__":
-  main()
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_socket.bind(('', server_port))
+  server_socket.listen(1)
+  
+  log.info("The server is ready to receive on port " + str(server_port))
+  
+  connections = {}
+  data = []
+  lock = threading.Lock()
+  i = 0
+  try:
+    while True:
+      if len(connections) > 1:
+        break
+        
+      client = server_socket.accept()
+      connections[i] = [client[0], client[1]]
+      threading.Thread(target = spawn_thread, args = (connections[i][0], connections[i][1], lock, data)).start()
+
+      i += 1
+  finally:
+    server_socket.close()
