8d25577f84f97080e265fe6f3a7da2410d9122b7
SMCaseyCode <104325289+SMCaseyCode@users.noreply.github.com>
2023-05-28 15:44:39 -0700

    Cleaned up + Comments

--- PA3/src/server.py
+++ PA3/src/server.py
@@ -25,39 +25,6 @@ message_list = []
 response = False
 
 
-def connection_handler(connection_socket):
-    # Read data from the new connectio socket
-    # Note: if no data has been sent this blocks until there is data
-    global response
-    message = connection_socket.recv(1024)
-    lt = time.localtime()
-
-    msg_timestamp = str(lt.tm_mon) + "/" \
-                    + str(lt.tm_mday) + "/" \
-                    + str(lt.tm_year) + " " \
-                    + str(lt.tm_hour) + ":" \
-                    + str(lt.tm_min) + ":" \
-                    + str(lt.tm_sec)
-
-    # Decode data from UTF-8 bytestream
-    message_decoded = message.decode()
-    message_list.append(message_decoded)
-    # Log query information
-    log.info("Received Message: \"" + str(message_decoded) + "\"" + " at " + msg_timestamp)
-
-    while not response:
-        if len(message_list) == 2:
-            # Perform some server operations on data to generate response
-            response = "X:" + "\'" + message_list[0] + "\'," \
-                       + " Y:" + "\'" + message_list[1] + "\'"
-
-            # Sent response over the network, encoding to UTF-8
-            connection_socket.send(response.encode())
-
-    # Close client socket
-    connection_socket.close()
-
-
 def main():
     # Create a TCP socket
     # Notice the use of SOCK_STREAM for TCP packets
@@ -72,6 +39,7 @@ def main():
     # Alert user we are now online
     log.info("The server is ready to receive on port " + str(server_port))
 
+    # Creates threads for connection. Currently set to 2 connections. While True loop makes this infinite.
     for i in range(2):
         connection_socket, address = server_socket.accept()
         t = Thread(target=thread_process, args=(connection_socket, address))
@@ -82,7 +50,38 @@ def thread_process(connection_socket, address):
 
     log.info("Connected to client at " + str(address))
 
-    connection_handler(connection_socket)
+    global response
+    message = connection_socket.recv(1024)
+
+    # Gets localtime of the delivered message. msg_timestamp places it into readable format
+    lt = time.localtime()
+
+    msg_timestamp = str(lt.tm_mon) + "/" \
+                    + str(lt.tm_mday) + "/" \
+                    + str(lt.tm_year) + " " \
+                    + str(lt.tm_hour) + ":" \
+                    + str(lt.tm_min) + ":" \
+                    + str(lt.tm_sec)
+
+    # Decode data from UTF-8 bytestream
+    message_decoded = message.decode()
+    # Adds message to list
+    message_list.append(message_decoded)
+
+    log.info("Received Message: \"" + str(message_decoded) + "\"" + " at " + msg_timestamp)
+
+    # Waits for both responses before creating response
+    while not response:
+        if len(message_list) == 2:
+            # Perform some server operations on data to generate response
+            response = "X:" + "\'" + message_list[0] + "\'," \
+                       + " Y:" + "\'" + message_list[1] + "\'"
+
+            # Sent response over the network, encoding to UTF-8
+            connection_socket.send(response.encode())
+
+    # Close client socket after response has been sent
+    connection_socket.close()
 
 
 if __name__ == "__main__":
