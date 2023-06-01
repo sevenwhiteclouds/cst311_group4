e639f8a5329d9e1c4de3a540b693231ecb33957a
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-05-31 18:22:04 -0700

    simpler because only 2 clients. x and y fix

--- PA3/src/server.py
+++ PA3/src/server.py
@@ -7,47 +7,39 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-server_port = 12000
+PORT = 12000
 
-def spawn_thread(connection_socket, address, lock, data):
+def spawn_thread(thread_num, client, address, data):
   log.info("Connected to client at " + str(address))
 
-  mssg = connection_socket.recv(1024).decode()
+  mssg = client.recv(1024).decode()
 
-  lock.acquire()
-  data.append(mssg)
-  lock.release()
-
+  if thread_num == 0:
+    data.append(f"X : \"{mssg}\"")
+  else:
+    data.append(f"Y : \"{mssg}\"")
+    
   log.info("Recieved query test \"" + mssg + "\"")
 
   while len(data) < 2:
     time.sleep(1)
   
-  response = 'X : "' + data[0] + '", Y : "' + data[1] + '"'
+  response = f"{data[0]}, {data[1]}"
 
-  connection_socket.send(response.encode())
-  connection_socket.close()
+  client.send(response.encode())
+  client.close()
 
 if __name__ == "__main__":
   server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server_socket.bind(('', server_port))
+  server_socket.bind(('', PORT))
   server_socket.listen(1)
   
-  log.info("The server is ready to receive on port " + str(server_port))
+  log.info("The server is ready to receive on port " + str(PORT))
   
-  connections = {}
   data = []
-  lock = threading.Lock()
-  i = 0
-  try:
-    while True:
-      if len(connections) > 1:
-        break
-        
-      client = server_socket.accept()
-      connections[i] = [client[0], client[1]]
-      threading.Thread(target = spawn_thread, args = (connections[i][0], connections[i][1], lock, data)).start()
 
-      i += 1
-  finally:
-    server_socket.close()
+  for i in range(2):
+    client, address = server_socket.accept()
+    threading.Thread(target = spawn_thread, args = (i, client, address, data)).start()
+
+  server_socket.close()
