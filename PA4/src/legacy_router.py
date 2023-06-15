bb5751428046dd9724126bcfcf2c017662a35e33
SMCaseyCode <104325289+SMCaseyCode@users.noreply.github.com>
2023-06-15 07:21:59 -0700

    21% loss, getting there.

--- PA4/src/legacy_router.py
+++ PA4/src/legacy_router.py
@@ -1,61 +1,59 @@
 #!/usr/bin/python
 
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import Host, Node
-from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.node import IVSSwitch
+from mininet.node import Controller, Node
+from mininet.node import OVSKernelSwitch
+from mininet.node import Host
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
 
 def myNetwork():
 
-  net = Mininet( topo=None,
-                 build=False,
-                 ipBase='10.0.0.0/24')
+  net = Mininet(topo=None, build=False, ipBase='10.0.0.0/24')
 
-  info( '*** Adding controller\n' )
-  c0=net.addController(name='c0',
-                       controller=Controller,
-                       protocol='tcp',
-                       port=6633)
+  info('*** Adding controller\n')
+  c0 = net.addController(name='c0',
+                         controller=Controller,
+                         protocol='tcp',
+                         port=6633)
 
-  info( '*** Add switches\n')
+  info('*** Add switches\n')
   s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
   s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
-  r5 = net.addHost('r5', cls=Node, ip='10.0.2.0/24')
+  r5 = net.addHost('r5', cls=Node, ip='10.0.2.1/24')
   r5.cmd('sysctl -w net.ipv4.ip_forward=1')
-  r4 = net.addHost('r4', cls=Node, ip='192.168.2.0/24')
+  r4 = net.addHost('r4', cls=Node, ip='192.168.1.1/24')
   r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-  r3 = net.addHost('r3', cls=Node, ip='10.0.1.0/24')
+  r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
   r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-  info( '*** Add hosts\n')
-  h1 = net.addHost('h1', cls=Host, ip='10.0.1.2', defaultRoute='via 10.0.1.0/24')
-  h2 = net.addHost('h2', cls=Host, ip='10.0.1.4', defaultRoute='via 10.0.1.0/24')
-  h3 = net.addHost('h3', cls=Host, ip='10.0.2.2', defaultRoute='via 10.0.2.0/24')
-  h4 = net.addHost('h4', cls=Host, ip='10.0.2.4', defaultRoute='via 10.0.2.0/24')
+  info('*** Add hosts\n')
+  h1 = net.addHost('h1', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+  h2 = net.addHost('h2', cls=Host, ip='10.0.1.4/24', defaultRoute='via 10.0.1.1')
+  h3 = net.addHost('h3', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
+  h4 = net.addHost('h4', cls=Host, ip='10.0.2.4/24', defaultRoute='via 10.0.2.1')
 
-  info( '*** Add links\n')
+  info('*** Add links\n')
   net.addLink(h1, s1)
   net.addLink(h2, s1)
   net.addLink(h3, s2)
   net.addLink(h4, s2)
-  net.addLink(s2, r5, intfName2='r5-eth1', params2={'ip':'10.0.2.3/24'})
-  net.addLink(s1, r3, intfName2='r3-eth1', params2={'ip':'10.0.1.3/24'})
-  net.addLink(r3, r4, intfName1='r3-eth0', params1={'ip':'192.168.1.2/24'}, intfName2='r4-eth0', params2={'ip':'192.168.1.4/24'})
-  net.addLink(r4, r5, intfName1='r4-eth1', params1={'ip':'192.168.3.4/24'}, intfName2='r5-eth0', params2={'ip':'192.168.3.6/24'})
+  net.addLink(s2, r5, intfName2='r5-eth1', params2={'ip': '10.0.2.3/24'})
+  net.addLink(s1, r3, intfName2='r3-eth1', params2={'ip': '10.0.1.3/24'})
+  net.addLink(r3, r4, intfName1='r3-eth0', params1={'ip': '192.168.1.2/24'}, intfName2='r4-eth0',
+              params2={'ip': '192.168.1.4/24'})
+  net.addLink(r4, r5, intfName1='r4-eth1', params1={'ip': '192.168.3.4/24'}, intfName2='r5-eth0',
+              params2={'ip': '192.168.3.6/24'})
 
-  info( '*** Starting network\n')
+  info('*** Starting network\n')
   net.build()
 
   info("1\n")
-  # r3 --> r4
-  info(net["r3"].cmd("ip route add 10.0.2.0/24 via 192.168.1.4 dev r3-eth0"))
-  # r4 --> r3 broken right now
+  # r4 --> r3
   info(net["r4"].cmd("ip route add 10.0.1.0/24 via 192.168.1.2 dev r4-eth0"))
+
+  # r3 --> r5
+  info(net["r3"].cmd("ip route add 10.0.2.0/24 via 192.168.1.1 dev r3-eth0"))
 
   info("2\n")
   # r5 --> r4
@@ -63,18 +61,19 @@ def myNetwork():
   # r4 --> r5
   info(net["r4"].cmd("ip route add 10.0.2.0/24 via 192.168.3.6 dev r4-eth1"))
 
-  info( '*** Starting controllers\n')
+  info('*** Starting controllers\n')
   for controller in net.controllers:
     controller.start()
 
-  info( '*** Starting switches\n')
+  info('*** Starting switches\n')
   net.get('s2').start([c0])
   net.get('s1').start([c0])
 
-  info( '*** Post configure switches and hosts\n')
+  info('*** Post configure switches and hosts\n')
   CLI(net)
   net.stop()
 
+
 if __name__ == '__main__':
-  setLogLevel( 'info' )
+  setLogLevel('info')
   myNetwork()
