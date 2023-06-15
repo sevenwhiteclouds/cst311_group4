1e5e64ef62feeb5f4beb1570a5859fe916a4fbea
SMCaseyCode <104325289+SMCaseyCode@users.noreply.github.com>
2023-06-14 23:12:39 -0700

    66% droppage

--- PA4/src/legacy_router.py
+++ PA4/src/legacy_router.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import Host, Node
 from mininet.node import OVSKernelSwitch, UserSwitch
 from mininet.node import IVSSwitch
 from mininet.cli import CLI
@@ -14,7 +14,7 @@ def myNetwork():
 
   net = Mininet( topo=None,
                  build=False,
-                 ipBase='10.0.0.0/8')
+                 ipBase='10.0.0.0/24')
 
   info( '*** Adding controller\n' )
   c0=net.addController(name='c0',
@@ -23,20 +23,20 @@ def myNetwork():
                        port=6633)
 
   info( '*** Add switches\n')
-  s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
   s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
-  r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/8')
-  r3.cmd('sysctl -w net.ipv4.ip_forward=1')
-  r4 = net.addHost('r4', cls=Node, ip='192.168.1.1/24')
-  r4.cmd('sysctl -w net.ipv4.ip_forward=1')
+  s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
   r5 = net.addHost('r5', cls=Node, ip='10.0.1.2/8')
   r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+  r4 = net.addHost('r4', cls=Node, ip='192.168.1.1/24')
+  r4.cmd('sysctl -w net.ipv4.ip_forward=1')
+  r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/8')
+  r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
   info( '*** Add hosts\n')
   h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute='10.0.1.1/8')
   h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute='10.0.1.1/8')
-  h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute='10.0.1.2/8')
-  h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute='10.0.1.2/8')
+  h3 = net.addHost('h1', cls=Host, ip='10.0.0.3', defaultRoute='10.0.1.2/8')
+  h4 = net.addHost('h2', cls=Host, ip='10.0.0.4', defaultRoute='10.0.1.2/8')
 
   info( '*** Add links\n')
   net.addLink(h1, s1)
@@ -66,4 +66,3 @@ def myNetwork():
 if __name__ == '__main__':
   setLogLevel( 'info' )
   myNetwork()
-
