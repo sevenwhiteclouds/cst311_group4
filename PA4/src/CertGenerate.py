b18aceddb301b94bbe547d325c144288f94a6009
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-06-16 05:38:26 -0700

    Create CertGenerate.py
    Created a Python script to generate both the web server and chat server's CA certificate and private key. The script makes a new folder called "ca-cert" where all the certificates are saved.

---
+++ PA4/src/CertGenerate.py
@@ -0,0 +1,25 @@
+import subprocess as sp
+import os
+
+# verify if folder "ca-cert" exists
+# no?, create it
+# yes?, delete it
+if os.path.exists("ca-cert"):
+    sp.call(["rm", "-rf", "ca-cert"])
+sp.call(["mkdir", "ca-cert"])
+
+
+# 1. generate a root CA certificate and private key for web server
+sp.call(['openssl', 'genrsa', '-out', 'ca-cert/webpa4.test-key.pem', '2048'])
+sp.call(['openssl', 'req', '-nodes', '--new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.webpa4.test"])
+
+
+
+# 2. generate a root CA certificate and private key for chat server
+sp.call(['openssl', 'genrsa', '-out', 'ca-cert/chatpa4.test-key.pem', '2048'])
+sp.call(['openssl', 'req', '-nodes', '--new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.chatpa4.test"])
+
+
+print("CA certificate and private key generated for : Web Server")
+print("CA certificate and private key generated for : Chat Server")
+
