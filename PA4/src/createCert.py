7a7434729e281137ba0af85e72198783f684fa56
puff82922 <skirchner@csumb.edu>
2023-06-16 22:04:07 -0700

    webserver certificate

--- PA4/src/createCert.py
+++ PA4/src/createCert.py
@@ -4,20 +4,14 @@ def create_cert(cn):
     # create key
     subprocess.run(["openssl", "genrsa", ",-out ",  f"{cn}-key.pem", " 2048"])
 
-    # create configuration file for cert attributes
-    config = f"""
-        [req]
-        distingushed_name = dn
-        """
-
     with open(f"{cn}", "w") as file:
-        file.write(config)
+        file.write()
 
     # generate cert signing request (CSR)
-    subprocess.run(["sudo openssl req -nodes -new -config /etc/ssl/openssl.cnf -key ", f"{cn}-key.pem", " -out ", f"{cn}"])
+    subprocess.run(["sudo openssl req -new -config /etc/ssl/openssl.cnf -key ", f"{cn}-key.pem", " -out ", f"{cn}.csr", "-subj", "/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN=www.cst311.test"])
 
     # generate self-signed certificate using the CSR
-    subprocess.run(["sudo openssl x509 -req -days 365 -in", f"{cn}.csr", "-CA cacert.pem -CAkey ./private/cakey.pem -CAcreateserial -out", f"{cn}-cert.pem"])
+    subprocess.run(["sudo openssl x509 -req -days 365 -in", f"{cn}.csr", "-CA /etc/ssl/demoCA/cacert.pem -CAkey /etc/ssl/demoCA/private/cakey.pem  -CAcreateserial -out", f"{cn}-cert.pem"])
 
 # asks for CN for the server
 cn = input("Input CN for the server: ")
