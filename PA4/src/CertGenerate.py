9d19d7e776fe0cc1a6a10277408231113a9293e5
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-06-16 21:32:45 -0700

    Update CertGenerate.py
    Added a folder & .txt to hold serial numbers of Certificates. Changed x509 server lines so now it works as like in lab. Thanks to making serial numbers.

--- PA4/src/CertGenerate.py
+++ PA4/src/CertGenerate.py
@@ -8,18 +8,29 @@ if os.path.exists("ca-cert"):
     sp.call(["rm", "-rf", "ca-cert"])
 sp.call(["mkdir", "ca-cert"])
 
+#Create and keep track of serial numbers
+sp.call(["mkdir", "ca-cert/serial"])
+sp.call(['touch', 'ca-cert/serial/index.txt'])
+f=open("ca-cert/serial/index.txt", "w+")
+f.write("1000")
+f.close
+
+
+# 1. generate a root CA signing certificate
+sp.call(['openssl', 'genrsa', '-aes256', '-out', 'cakey.pem', '2048'])
+sp.call(['openssl', 'req', '-x509', '-new', '-nodes', '-key','cakey.pem', '-sha256', '-days', '1825', '-out','cacert.pem', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.ca.csumb.test"])
 
 # 1. generate a root CA certificate and private key for web server
 sp.call(['openssl', 'genrsa', '-out', 'ca-cert/webpa4.test-key.pem', '2048'])
-sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr.pem', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.webpa4.test"])
-sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/webpa4.test.csr.pem',  '-signkey', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test-cert.crt' ])
+sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.webpa4.test"])
+sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/webpa4.test.csr',  '-CA', 'cacert.pem', '-CAkey', 'cakey.pem', '-CAcreateserial', '-out', 'ca-cert/webpa4.test-cert.pem' ])
 print("\n")
 
 
 # 2. generate a root CA certificate and private key for chat server
 sp.call(['openssl', 'genrsa', '-out', 'ca-cert/chatpa4.test-key.pem', '2048'])
-sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr.pem', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.chatpa4.test"])
-sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/chatpa4.test.csr.pem',  '-signkey', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test-cert.crt' ])
+sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.chatpa4.test"])
+sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/chatpa4.test.csr',  '-CA', 'cacert.pem', '-CAkey', 'cakey.pem', '-CAcreateserial', '-out', 'ca-cert/chatpa4.test-cert.pem' ])
 print("\n")
 
 print("CA certificate and private key generated for : Web Server")
