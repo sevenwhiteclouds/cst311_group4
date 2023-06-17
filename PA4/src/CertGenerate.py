55f6f395228750d7f07caa0f087370e3d28e25c3
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-06-16 22:54:13 -0700

    Update CertGenerate.py
    Little aesthetic changes

--- PA4/src/CertGenerate.py
+++ PA4/src/CertGenerate.py
@@ -12,8 +12,8 @@ sp.call(["mkdir", "ca-cert"])
 print("\nGenerating Root CA Certificate for Web Server\n========================================\n")
 sp.call(['openssl', 'genrsa', '-out', 'ca-cert/webpa4.test-key.pem', '2048'])
 cn1 = input("Input CN for the web server: ")
-s = f"/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN={cn1}"
-sp.call(['openssl', 'req', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr', '-subj', s])
+s1 = f"/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN={cn1}"
+sp.call(['openssl', 'req', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr', '-subj', s1])
 sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/webpa4.test.csr',  '-CA', '/etc/ssl/demoCA/cacert.pem', '-CAkey', '/etc/ssl/demoCA/private/cakey.pem', '-CAcreateserial', '-out', 'ca-cert/webpa4.test-cert.pem' ])
 print("\n")
 
@@ -21,13 +21,13 @@ print("\n")
 # 2. generate a root CA certificate and private key for chat server
 print("Generating Root CA Certificate for Chat Server\n========================================\n")
 sp.call(['openssl', 'genrsa', '-out', 'ca-cert/chatpa4.test-key.pem', '2048'])
-cn2 = input("Input CN for the web server: ")
-s = f"/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN={cn2}"
-sp.call(['openssl', 'req', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr', '-subj', s])
+cn2 = input("Input CN for the chat server: ")
+s2 = f"/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN={cn2}"
+sp.call(['openssl', 'req', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr', '-subj', s2])
 sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/chatpa4.test.csr',  '-CA', '/etc/ssl/demoCA/cacert.pem', '-CAkey', '/etc/ssl/demoCA/private/cakey.pem', '-CAcreateserial', '-out', 'ca-cert/chatpa4.test-cert.pem' ])
 print("\n")
 
-
+# print what was generated
 print("CA certificate and private key generated for : Web Server")
 print("CA certificate and private key generated for : Chat Server")
 print("\n")
