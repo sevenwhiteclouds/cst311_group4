a1f4070890c4a5cc0d3f112c4a61a6f3298aa73d
ThisIs5am <ThisIs5am@users.noreply.github.com>
2023-06-16 20:21:15 -0700

    Update CertGenerate.py
    remove aes256

--- PA4/src/CertGenerate.py
+++ PA4/src/CertGenerate.py
@@ -10,14 +10,14 @@ sp.call(["mkdir", "ca-cert"])
 
 
 # 1. generate a root CA certificate and private key for web server
-sp.call(['openssl', 'genrsa', '-aes256', '-out', 'ca-cert/webpa4.test-key.pem', '2048'])
+sp.call(['openssl', 'genrsa', '-out', 'ca-cert/webpa4.test-key.pem', '2048'])
 sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test.csr.pem', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.webpa4.test"])
 sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/webpa4.test.csr.pem',  '-signkey', 'ca-cert/webpa4.test-key.pem', '-out', 'ca-cert/webpa4.test-cert.crt' ])
 print("\n")
 
 
 # 2. generate a root CA certificate and private key for chat server
-sp.call(['openssl', 'genrsa', '-aes256', '-out', 'ca-cert/chatpa4.test-key.pem', '2048'])
+sp.call(['openssl', 'genrsa', '-out', 'ca-cert/chatpa4.test-key.pem', '2048'])
 sp.call(['openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test.csr.pem', '-subj', "/C=US/ST=CA/L=Corona/O=CST311/OU=Networking/CN=www.chatpa4.test"])
 sp.call(['openssl', 'x509', '-req', '-days', '365', '-in', 'ca-cert/chatpa4.test.csr.pem',  '-signkey', 'ca-cert/chatpa4.test-key.pem', '-out', 'ca-cert/chatpa4.test-cert.crt' ])
 print("\n")
